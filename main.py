6f580e8d14ab64fdcfa220b68d661d58b69ab980
Комп <elizabethsoftware27@gmail.com>
2022-11-25 23:50:18 +1000

    2

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
 class Window(QMainWindow):
-    def __init__(self):
+    def __init__(self, ed):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.connection = sqlite3.connect("coffee.db")
         self.pushButton.clicked.connect(self.show_table)
         self.textEdit.setPlainText("SELECT * FROM coffee")
+        self.pushButton_2.clicked.connect(ed.show)
 
     def show_table(self):
         result = self.connection.cursor().execute(self.textEdit.toPlainText())
@@ -27,8 +28,28 @@ class Window(QMainWindow):
                     i, j, QTableWidgetItem(str(elem)))
 
 
+class Edit(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.connection = sqlite3.connect("coffee.db")
+        self.pushButton.clicked.connect(self.edit)
+        self.textEdit.setPlainText("INSERT INTO coffee('id','Sort Name','Roasting degree','Ground/whole bean','Taste',"
+                                   "'Price','Packing volume') VALUES(2,'Liberica','Middle','whole bean','???',300,400)")
+
+    def edit(self):
+        self.connection.cursor().execute(self.textEdit.toPlainText())
+        self.connection.commit()
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Window()
+    ed = Edit()
+    ex = Window(ed)
+    sys.excepthook = except_hook
     ex.show()
     sys.exit(app.exec())
